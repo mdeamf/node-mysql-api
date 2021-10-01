35b79ccdfc2c921732ccc6249d38c3628018c957
mdeamf <mauriciodealmeidamf@gmail.com>
2021-10-01 15:13:59 -0300

    feat: post creation

--- routes/posts.js
+++ routes/posts.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const postsController = require('../controllers/post.controller');
 
-router.get('/', postsController.index);
+router.post('/', postsController.save);
 
 module.exports = router;
